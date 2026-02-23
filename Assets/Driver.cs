d05d9ee7ba693a6628c85bf4b7d64638efaa7402
Barthz <ismaelrb@gmail.com>
2026-02-24 00:09:18 +0100

    9. Transform 10. Variables 11. SerializeField 12. Keyboard & If 13. Moving 14. deltaTime

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -1,16 +1,36 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Driver : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] float moveSpeed = 10f;
+   
+    [SerializeField]float steerSpeed = 200f;
 
     // Update is called once per frame
     void Update()
     {
+        float move = 0f;
+        float steer = 0f;
 
+        if (Keyboard.current.wKey.isPressed || Keyboard.current.upArrowKey.isPressed) {
+            move = 1f;
+        } else if (Keyboard.current.sKey.isPressed || Keyboard.current.downArrowKey.isPressed) {
+            move = -1f;
+        }
+
+        if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed) {
+            steer = 1f;
+        } else if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed) {
+            steer = -1f;
+        }
+
+        float moveAmount = move * moveSpeed * Time.deltaTime;
+        float steerAmount = steer * steerSpeed * Time.deltaTime;
+
+        transform.Translate(0, moveAmount, 0);
+        transform.Rotate(0, 0, steerAmount );
+
+        Debug.Log("Move Speed: " + moveAmount + " Steer Amount: " + steerAmount);
     }
 }
