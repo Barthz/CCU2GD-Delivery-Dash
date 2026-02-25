bd7239009ab02b9a2dd8e8a8206e920cc2f081c4
Barthz <ismaelrb@gmail.com>
2026-02-25 13:36:29 +0100

    24. Bools

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -5,17 +5,21 @@ public class Delivery : MonoBehaviour
     const string PACKAGE_TAG = "Package";
     const string CUSTOMER_TAG = "Customer";
 
+    private bool hasPackage;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         // if the tag is package, then do something
         if (collision.CompareTag(PACKAGE_TAG))
         {
             Debug.Log("Package " + collision.gameObject.name + " collected!");
+            hasPackage = true;
         }
 
-        if (collision.CompareTag(CUSTOMER_TAG))
+        if (collision.CompareTag(CUSTOMER_TAG) && hasPackage)
         {
             Debug.Log("Package delivered to " + collision.gameObject.name + "!");
+            hasPackage = false;
         }
     }
 }
