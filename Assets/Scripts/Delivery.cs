6fb584760e675c731bd53e4f7d79ffb4d083845a
Barthz <ismaelrb@gmail.com>
2026-02-26 18:35:20 +0100

    29. UI Text

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -24,6 +24,7 @@ public class Delivery : MonoBehaviour
             Debug.Log("Package delivered to " + collision.gameObject.name + "!");
             hasPackage = false;
             GetComponent<ParticleSystem>().Stop();
+            Destroy(collision.gameObject, destroyDelay);
         }
     }
 }
--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -1,28 +1,45 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class Driver : MonoBehaviour
 {
     private const string BOOST_TAG = "Boost";
+    private const string WORLD_COLLISION_TAG = "WorldCollision";
 
     [SerializeField] float currentSpeed = 5f;
     [SerializeField] float boostSpeed = 10f;
     [SerializeField] float regularSpeed = 5f;
-
     [SerializeField] float steerSpeed = 200f;
 
-    void OnCollisionEnter2D(Collision2D collision)
+    [SerializeField] TMP_Text boostText;
+
+    void Start()
     {
-        currentSpeed = regularSpeed;
+        boostText.gameObject.SetActive(false);
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag(BOOST_TAG))
         {
+            Debug.Log("Boost collected! Speed increased.");
+
             currentSpeed = boostSpeed;
+            boostText.gameObject.SetActive(true);
             Destroy(collision.gameObject);
         }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag(WORLD_COLLISION_TAG))
+        {
+            Debug.Log("Crashed! Boost reset.");
+
+            currentSpeed = regularSpeed;
+            boostText.gameObject.SetActive(false);
+        }   
     }
 
     // Update is called once per frame
